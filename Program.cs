bf412ea535b1d24cb7422322041a4fb2cbe6a543
Ing.Samuel <126287892+SameuelxD@users.noreply.github.com>
2023-09-18 01:10:14 +0000

    zanahoria

--- Program.cs
+++ Program.cs
@@ -1,66 +1,110 @@
-﻿using System.Collections;
-using Microsoft.VisualBasic;
+﻿using System;
+using System.Collections.Generic;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        /*Console.WriteLine(Math.Max(45,101)); /* La clase Math con metodo Max retorna el numero mas alto de los argumentos max = 101 
-        Console.WriteLine(Math.Min(45,101)); /* La clase Math con metodo Min retorna el numero mas bajo de los argumentos min = 45 
-        Console.WriteLine(Math.Pow(5,3)); /* La clase Math con metodo Pow coge el primer numero y lo eleva su segundo como exponente 5**3 = 125 
-        Console.WriteLine(Math.Sqrt(16)); /* La clase Math con metodo Sqrt retorna la raiz de dicho numero 4 
-        Console.WriteLine(Math.Sqrt(17)); /* La clase Math con metodo Sqrt tambien retornara decimales si es necesaario 4.123105 
-        Console.WriteLine(Math.PI); /* Retorna el valor de PI 
-
-        double numero = 3.4681;
-        Console.WriteLine(Math.Round(numero)); /* Round evalua  si el numero en que comienza la parte decimal es >=5 lo reondea una unidad , si es menor lo redondeara para abajo 
-        Console.WriteLine(Math.Ceiling(numero)); /* Ceiling siempre redondeara cualquier valor de mayor o menor de 5 para arriba le va a agregar una unidad si es un decimal cualquiera 
-        Console.WriteLine(Math.Floor(numero)); /* Floor siempre redondeara cualquier valor mayor o menor de 5 para abajo le va a restar una unidad si es un decimal cualquiera
-        Console.WriteLine(Math.Abs(-123)); /* Retornara el valor absoluto es decir siempre un numero positivio */
-        List<String> Materias = new List<String>();
-        int idx;
-        Materias.Add("Calculo");
-        Materias.Add("Español");
-        Materias.Add("Dibujo Tecnico");
-        Materias.Add("Ingles");
-        ContarLista(Materias);
-        Console.WriteLine("Ingrese la posicion {0}");
-        idx=Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("El elemento es el indice {0} es: {1} ", idx,ObtenerElemento(idx,Materias));
-        Materias.Add(AddItemData());
-        Console.WriteLine("Ingrese la palabra a buscar");
-        if(ValidarItem(Materias,Console.ReadLine().ToLower())){
-            Console.WriteLine("Ok , lo Encontre");
-        }else{
-            Console.WriteLine("No lo encontre");  // Los metodos de una clase sirven para definir comportamientos de la misma y me permite modificar su uso 
+        Dictionary<string, string> openWith = new Dictionary<string, string>(); /* Declaracion de un nuevo diccionario */
+        openWith.Add("txt", "notepad.exe");
+        openWith.Add("bmp", "paint.exe");
+        openWith.Add("dib", "paint.exe");
+        openWith.Add("rtf", "wordpad.exe");
+        bool flag=true;
+        while(flag){
+            Console.Clear();
+            Console.WriteLine("-------------------- MENU --------------------");
+            Console.WriteLine("   1. Buscar elemento con la llave. ");
+            Console.WriteLine("   2. Mostrar Valores y Claves. ");
+            Console.WriteLine("   3. Mostrar Valores. ");
+            Console.WriteLine("   4. Agregar Elementos. ");
+            Console.WriteLine("   5. Limpiar Consola Clear. ");
+            Console.WriteLine("   6. ContainsKey. ");
+            Console.WriteLine("   7. ContainsValue. ");
+            Console.WriteLine("   8. Remove. ");
+            Console.WriteLine("Ingrese la opcion: ");
+            int opcion=int.Parse(Console.ReadLine());
+            switch(opcion)
+            {
+                case 1:
+                    Console.Clear();
+                    try
+                    {
+                        openWith.Add("txt", "winword.exe");
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine("Un elemento con la llave = \"txt\" ya existe.");
+                    }
+                    
+                    Console.WriteLine("El total de los elementos del diccionario es: {0}", openWith.Count);
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    Console.Clear();
+                    Dictionary<string,string>.KeyCollection keyColl = openWith.Keys; 
+                    foreach(string key in keyColl){
+                        Console.WriteLine(key);
+                    }
+                    foreach(KeyValuePair<string,string> pair in openWith){
+                        Console.WriteLine("Llave {0} - Valor {1}",pair.Key,pair.Value);
+                    }
+                    Console.ReadLine();
+                    break;
+                case 3:
+                    Console.Clear();
+                    Dictionary<string,string>.ValueCollection valueColl=openWith.Values;
+                    foreach(string valor in valueColl){
+                        Console.WriteLine(valor);
+                    }
+                    Console.ReadLine();
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("Ingrese el valor y la clave para agregar al diccionario: ");
+                    string keys=Console.ReadLine();
+                    string values=Console.ReadLine();
+                    openWith.Add(keys,values);
+                    break;
+                case 5:
+                    Console.Clear();
+                    Console.WriteLine("Elementos antes del Clear: {0}", openWith.Count);
+                    Console.WriteLine("Limpiando Consola");
+                    Console.WriteLine("Elementos despues del Clear: {0}", openWith.Count);
+                    openWith.Clear();
+                    break;
+                case 6:
+                    if(openWith.ContainsKey("bmp")){
+                        Console.WriteLine("El tipo de formato esta soportado.");
+                    }
+                    Dictionary<string,string>.ValueCollection valueCol=openWith.Values;
+                    foreach(string val in valueCol){
+                        Console.WriteLine(val);
+                    }
+                    Console.ReadLine();
+                    break;
+                case 7:
+                    if(openWith.ContainsValue("notepad")){
+                        Console.WriteLine("El valor se encuentra registrado.");
+                    }else{
+                        Console.WriteLine("El valor no se encuentra registrado- ");
+                    }
+                    Dictionary<string,string>.ValueCollection valorColl=openWith.Values;
+                    foreach(string valor in valorColl){
+                        Console.WriteLine(valor);
+                    }
+                    Console.ReadLine();
+                    break;
+                    case 8:
+                        Console.WriteLine("Ingrese el elemento a elininar: ");
+                        string elemet= Console.ReadLine();
+                        openWith.Remove(elemet);
+                        break;
+                default:
+                    Console.Clear();
+                    flag=false;
+                    break;
+            }
         }
-        string dino;
-        string[] Dinos = {
-                            "Brachiosaurus","Amargasaurus","Mamenchisaurus"                    
-                            };
-        Console.WriteLine("Ingrese el nombre del Dinosairoio a buscar: ");
-        dino=Console.ReadLine();
-        string ? nombre = Dinos.Find(n => n.StartsWith(palabra ?? String.Empty));
-        Console.WriteLine(nombre != null ? "Se encontro el Dinosaurio" : "No se encontro el Dinosaurio");
-    }
-    public static void ContarLista(List<String> lista){
-        Console.WriteLine("Total elementos de la lista {0} " , lista.Count());
-    }
-    public static string AddItemData(){
-        Console.WriteLine("Ingrese el titulo del elemento");
-        return Console.ReadLine();
-    }
-
-    /// <summary>
-    /// Metodo para obtener el elemento de una posicion de una lista generica
-    /// </summary>
-    /// <param name="idx"></param>
-    /// <param name="lista"></param>
-    /// <returns></returns>
-    public static string ObtenerElemento(int idx,List<String> lista){
-        return lista[idx];
-    }
-    public static bool ValidarItem(List<String> lista,string valor){
-        return lista.Exists(l => l.ToLower().Equals(valor));
     }
 }
